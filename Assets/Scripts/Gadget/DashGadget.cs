078e136e9982333de021bf9cf5932f16aa7542b9
eProw <jasansu@gmail.com>
2021-11-30 19:26:06 +0100

    Arreglar movimiento del dash (No salir de las paredes)
    Se suprime la posibilidad de teletransportarse. En su lugar, lo que hará sería ir muy rápido de un punto a otro

--- Assets/Scripts/Gadget/DashGadget.cs
+++ Assets/Scripts/Gadget/DashGadget.cs
@@ -76,10 +76,10 @@ public class DashGadget : Gadget
         }
 
 
-        if (Vector2.Distance(destPosition, transform.position) > maxDistance * 2)
+        /*if (Vector2.Distance(destPosition, transform.position) > maxDistance * 2)
         {
             destPosition = transform.position;
-        }
+        }*/
 
         StartCoroutine(DashCoroutine());
     }
@@ -91,13 +91,29 @@ public class DashGadget : Gadget
         Vector3 auxPos = transform.position + (Vector3)(orientation * maxDist);
         Vector3 finalPos;
 
-        if (Physics2D.OverlapCircle(auxPos, 3f))
+        //Si resulta que en el lugar de reaparición existe un obstáculo o no es una coordenada válida,
+        //se deberá buscar el último punto de colisión válido para reaparecer ahí
+        if (true)//Physics2D.OverlapCircle(auxPos, 3f))
         {
+
+            //Usaremos esta opción en caso de que favorezca a la movilidad:
+
+            // - Encontrar todos los puntos de colisión
+            // - Iterar hasta encontrar el punto más lejano que se encuentre dentro de una cueva conocida
+            // - (Si la cueva mencionada es conocida, habrá que activar su trigger de entrada o salida, ya que
+            // reaparecer ahí es como si hubiera entrado en la cueva)
+
+            /*RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, orientation, maxDist, collisionMask);
+            System.Array.Sort(rays, (x, y) => x.distance.CompareTo(y.distance));*/
+
             RaycastHit2D ray = Physics2D.Raycast(transform.position, orientation, maxDist, collisionMask);
 
             if (ray.point != Vector2.zero)
             {
-                finalPos = ray.point - orientation * 4; //Offset para no aparecer dentro de la pared
+                //Necesitamos que exista cierto offset, ya que no podemos situar el punto de reaparición
+                //justo donde el rayo choca, ya que existe la posibilidad de que el jugador se encuentre
+                //en una esquina y, por accidente, la atraviese debido a la escasez de pared entre dos muros.
+                finalPos = ray.point - orientation * 8;
             }
             else
             {
